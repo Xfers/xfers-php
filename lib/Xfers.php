448395ee710f2c15a4dc3ff95b70410affba0874
seowyanyi <seow.yanyi@gmail.com>
2016-11-04 13:28:45 +0800

    bump version

--- lib/Xfers.php
+++ lib/Xfers.php
@@ -19,7 +19,7 @@ class Xfers
     const IDproductionBase = 'https://id.xfers.com/api/v3';
 
     public static $apiBase = "";
-    const VERSION = '1.1.1';
+    const VERSION = '1.2.0';
 
     /**
      * @return string The API key used for requests.
